022f14e802615fb375ff21dcc7302ec5bc1c1a04
Andrei Dobysh <andrei.dobysh@elteam-group.com>
2019-07-08 21:22:58 +0300

    Initial Commit

---
+++ IGHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGHelper
+//
+//  Created by Andrei Dobysh on 7/8/19.
+//  Copyright © 2019 Andrei Dobysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
