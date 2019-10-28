83bf3a04cbe1592ad9c1010cf8b5d555a9a443a4
Andrei Dobysh <andrei.dobysh@elteam-group.com>
2019-10-29 00:37:27 +0300

    func that split array in two halfs

--- IGHelper/Extension/ArrayExtension.swift
+++ IGHelper/Extension/ArrayExtension.swift
@@ -30,4 +30,11 @@ extension Array {
         self = shuffledElements
     }
     
+    func split() -> [[Element]] {
+        let half = count / 2
+        let leftSplit = self[0 ..< half]
+        let rightSplit = self[half ..< count]
+        return [Array(leftSplit), Array(rightSplit)]
+    }
+    
 }
